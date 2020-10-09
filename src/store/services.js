6326d01335c97f5a9df71dc72ffad539b410999d
Crowdbotics <team@crowdbotics.com>
2020-10-09 00:40:30 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -3,6 +3,10 @@ const dogAPI = axios.create({
   baseURL: "https://dog.ceo/api/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+const newConnectorhjgfhjg = axios.create({
+  baseURL: "https://app.crowdbotics.com/dashboard/app/20130/storyboard/99273/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function dogapi_get_breeds_image_random_read(action) {
   return dogAPI.get(`/breeds/image/random`)
 }
